7d481d974ae8bb2dacfd941f7bfe532d38c600c5
Valtemir Procópio de Lima <valtemir.procopio@rn.senac.br>
2023-01-20 09:27:38 -0300

    Template

--- app/routes.py
+++ app/routes.py
@@ -7,10 +7,14 @@ from flask import render_template
 def index():
     return render_template('index.html')
 
-@app.route('/contato.html')
+@app.route('/contato')
 def contato():
     return render_template('contato.html')
 
-@app.route('/features.html')
+@app.route('/features')
 def features():
     return render_template('features.html')
+
+@app.route('/teste')
+def teste():
+    return render_template('teste.html')
